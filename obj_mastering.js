14ffcafaa0f193b7229d733f2cc31418a5bbe834
Al <alex2015mig@gmail.com>
2020-08-16 15:12:57 +0300

    mess

---
+++ obj_mastering.js
@@ -0,0 +1,134 @@
+const users = [
+    {
+        name: 'Alex',
+        isMale: true,
+        date: '14.08.1999'
+    },
+    {
+        name: 'John',
+        isMale: true,
+        date: '23.04.2000'
+    },
+    {
+        name: 'Alice',
+        isMale: false,
+        date: '08.07.2003'
+    },
+    {
+        name: 'Liam',
+        isMale: true,
+        date: '28.12.2005'
+    },
+    {
+        name: 'Emma',
+        isMale: false,
+        date: '09.04.2000'
+    },
+    {
+        name: 'Olivia',
+        isMale: false,
+        date: '19.12.2004'
+    },
+    {
+        name: 'Ethan',
+        isMale: true,
+        date: '02.11.2003'
+    },
+];
+
+function reversed(obj) {
+    const reversedObj = Object.entries(obj).reduce((reversed, entries) => {
+        const key = entries[0];
+        const val = entries[1];
+
+        reversed[val] = key;
+
+        return reversed;
+    }, {});
+
+    return reversedObj;
+}
+
+const rev = users.map()
+
+const reversedUsers = users.reduce((result, users) => {
+
+}, []);
+
+users.forEach((item, idx) => {
+    item.index = idx;
+});
+
+const counter = users.reduce(whoIsMore, {
+    male: 0,
+    female: 0
+});
+
+function whoIsMore(previous, current, i, arr) {
+    if (current.isMale) {
+        previous.male++;
+    } else {
+        previous.female++;
+    }
+
+    return previous;
+}
+
+const maleCount = counter.male;
+const femaleCount = counter.female;
+
+const message = maleCount > femaleCount ? 'Мужчин больше чем женщин' :
+    femaleCount > maleCount ? 'Женщин больше чем мужчин' : 'Равно';
+console.log(message);
+
+const array = ['ololo', 'anna', 'red', 'ololo', 'qwe', 'anna', 'ololo'];
+
+const uniqueObj = array.reduce(createObj, {});
+
+function createObj(previousValue, currentKey) {
+    if (currentKey in previousValue) {
+        previousValue[currentKey]++;
+    } else {
+        previousValue[currentKey] = 1;
+    }
+
+    return previousValue;
+}
+
+console.log(uniqueObj);
+
+const object = {
+    name: 'Olivia',
+    age: 20,
+}
+
+const newObject = reverseObj(object);
+
+function reverseObj(object) {
+    const newObject = {};
+    for (const [key, val] of Object.entries(object)) {
+        newObject[val] = key;
+    }
+    return newObject;
+}
+
+console.log(newObject);
+
+//это не относиться к заданию
+function congratulate(user) {
+
+    const date = user.date.split('.');
+
+    const lessThanWeek = new Date(+date[2], +date[1], +date[0]);
+
+    const now = new Date();
+
+    if (lessThanWeek.getMonth() === now.getMonth() + 1) {
+        const temp = now.getDate() - lessThanWeek.getDate();
+        if (temp >= 0 && temp <= 7) {
+            console.log(`\nHappy birthday to ${user.name} :)`);
+        }
+    }
+}
+
+congratulate(users[0]);
